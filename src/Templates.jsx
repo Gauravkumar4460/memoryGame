5582a82c4daf9ac0767f65eaba94bb5299afffc0
Gauravkumar4460 <gauravkr194460@gmail.com>
2024-08-20 15:11:27 +0530

    Initial commit

--- src/Templates.jsx
+++ src/Templates.jsx
@@ -58,7 +58,7 @@ function GridItem({onclick,id}){
 
     useEffect(()=> {
         async function getGif() {
-            const apiKey = '5OLXZKzOpuAYKAsVWMv7JqNHrkyybQ6u'; 
+            const apiKey = 'UgRR4FqbrAMQcBGNXlJ6AsWKXeXNEbZZ';
             const limit = 16;
             const query = 'black butler';
             const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=${limit}&rating=g`;
