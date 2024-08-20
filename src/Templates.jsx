f076c512a719a6f39ad3a92940ed890ba42ad7f1
Gauravkumar4460 <gauravkr194460@gmail.com>
2024-08-20 14:37:31 +0530

    corrected file location in html file

--- src/Templates.jsx
+++ src/Templates.jsx
@@ -58,7 +58,7 @@ function GridItem({onclick,id}){
 
     useEffect(()=> {
         async function getGif() {
-            const apiKey = '7Ag9E6IefsvOA6Ry4TedcdxOOTWjcKS8'; 
+            const apiKey = '5OLXZKzOpuAYKAsVWMv7JqNHrkyybQ6u'; 
             const limit = 16;
             const query = 'black butler';
             const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=${limit}&rating=g`;
