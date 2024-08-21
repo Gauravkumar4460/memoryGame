b9d9306cc7b23b02b79f4487233d016f4f7c0298
Gauravkumar4460 <gauravkr194460@gmail.com>
2024-08-21 10:12:10 +0530

    added the game over section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { FrontPage } from './Templates.jsx';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({onClick}) {
+    const [gameOver,setGameOver] = useState(false);
     const [counter,setCounter] = useState(0);
     const [selectedImageId,setSeletedImageId] = useState([]);
-
     
     const addImageId = (id) => {
       setSeletedImageId(prevItem => [...prevItem,id]);
@@ -13,16 +14,25 @@ function App() {
     }
     function handleImageClick(e){
       if(selectedImageId.find(id =>  id === e.target.id)){
-            setCounter(0);
-            setSeletedImageId([]);
+            setGameOver(true);
       }else{
         setCounter(counter+1);
         addImageId(e.target.id);
       }
     }
+    if(gameOver){
+      return (
+        <div style={{fontFamily:'cursive',fontSize:'xx-larger',display:'flex', flexDirection:'column', gap:'50px', justifyContent:'center', alignItems:'center', backgroundColor:'beige', width:'80vw', height:'80vh',margin:'50px auto'}}>
+        <h1 style={{fontFamily:'cursive', fontSize:'xx-larger'}}>Score : {counter}</h1>
+        <button onClick={onClick} className='playAgain-button'> play Again </button>
+        </div>
+      )
+    }
     return(
       <FrontPage handleImgClick={handleImageClick} counter={counter}/>
     )
 }
-
+App.propTypes = {
+  onClick : PropTypes.func.isRequired,
+}
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function handleClick() {
+    createRoot(document.getElementById('root')).render(
+        <App onClick={handleClick}/>
+    )
+}
 createRoot(document.getElementById('root')).render(
-    <App />
+    <App onClick={handleClick}/>
 )
